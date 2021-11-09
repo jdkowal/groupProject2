db52db19e4f609eaa4dced5b503ba9dc18b797b0
j-goldrath <jonathan.goldrath@gmail.com>
2021-11-09 11:41:47 -0800

    added register.js file to public/js to new user registrations

---
+++ public/js/register.js
@@ -0,0 +1,30 @@
+const registerFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    // Gather the data from the form elements on the page
+    const firstName = document.querySelector('#firstname-register').value.trim();
+    const lastName = document.querySelector('#lastname-register').value.trim();
+    const email = document.querySelector('#email-register').value.trim();
+    const password = document.querySelector('#password-register').value.trim();
+  
+    if (firstName && lastName && email && password) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/users/register', {
+        method: 'POST',
+        body: JSON.stringify({ firstName, lastName, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.register-form')
+    .addEventListener('submit', registerFormHandler);
+  
