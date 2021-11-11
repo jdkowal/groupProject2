53505d19c36159b5770f0f8ba9bee398ef24d799
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-11-11 17:38:43 -0600

    no event listener but added a new route

--- controllers/index.js
+++ controllers/index.js
@@ -5,10 +5,12 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const medRoutes = require('./medicationRoutes');
 const userCoupon = require('./userCouponRoutes');
+const qrCode = require('./qrRoute');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/medication', medRoutes);
 router.use('/coupon', userCoupon);
+router.use('/qr-code', qrCode)
 
 module.exports = router;
