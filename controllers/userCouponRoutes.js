4c8ea30554a318d3715301ffaa1882e3459ac7a1
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-11-12 10:52:32 -0600

    ok

--- controllers/userCouponRoutes.js
+++ controllers/userCouponRoutes.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res)=> {
-    let userCouponPage = await axios.get(`/api/coupon/${req.session.user_id}`)
+    let userCouponPage = await axios.get(`https://shielded-mountain-17631.herokuapp.com:${process.env.PORT}/api/coupon/${req.session.user_id}`)
 userCouponPage = userCouponPage.data
     console.log(userCouponPage);
 res.render('coupons', {userCouponPage, logged_in: req.session.logged_in})
